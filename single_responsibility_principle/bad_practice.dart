86097383ee956f1d3b2a56bff83784351bb6ed43
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2024-09-04 15:36:48 +0300

    single_responsibility_principle

---
+++ single_responsibility_principle/bad_practice.dart
@@ -0,0 +1,13 @@
+
+class User {
+  late int id;
+  late String name;
+
+  void save() {
+    // save user to database
+  }
+
+  void sendEmail() {
+    // send email to user
+  }
+}
---
+++ single_responsibility_principle/good_practice.dart
@@ -0,0 +1,17 @@
+
+class User {
+  late int id;
+  late String name;
+}
+
+class UserRepository {
+  void save(User user) {
+    // save user to database
+  }
+}
+
+class EmailService {
+  void sendEmail(User user) {
+    // send email to user
+  }
+}
